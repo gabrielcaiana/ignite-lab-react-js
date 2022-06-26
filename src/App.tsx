4889c0d3570cc45e9a76f1b125a56d0cccb24ddd
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-06-26 02:27:34 -0300

    config apollo and graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
+import { gql, useQuery } from '@apollo/client'
+
+const GET_LESSON_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
 function App() {
+  const { data } = useQuery<{ lessons: Lesson[]}>(GET_LESSON_QUERY)
+
   return (
-    <h1 className="text-red-500">hello world</h1>
+    <div className="p-4">
+      {data?.lessons.map(lesson => {
+        return <li key={lesson.id}> {lesson.title} </li>
+      })}
+    </div>
   )
 }
 
